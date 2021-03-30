51d3eb7e32e45cf974899dc8add6592548984cb3
Eric Tseng <erictseng89@gmail.com>
2021-03-30 23:41:34 +0800

    finished all basic layout and code for pair puzzle

--- js/test8.js
+++ js/test8.js
@@ -1,48 +1,74 @@
-let cardNumber = 52;
+const cardNumber = 52;
 let cardBank = [];
 let tempBank = [];
-let cardPlay = 24;
+let cardPairs = 12;
+let cardPlay = cardPairs * 2;
+let hasLoaded = false;
 let hasShuffled = false;
+let isBusy = false;
+let flip = false;
+let card1= 0;
+let card2= 0;
+let hasStarted = false;
+let timer1 = 0;
+let timer2 = 0;
+let elapsedTime = 0;
+let winCount = 0;
 
-function askCard() {
-    do {
-        cardPlay = prompt("要玩幾張?\n(2 ~ 52)", 4);
-    }
-    while (cardPlay < 2 || cardPlay > 52);
-
-    for (i = 0; i < cardPlay; i++) {
-        let imgMake = "<div class='cardBox'><img id='one' class='cardClass' name='cardName' onclick='check(" + i + ")' src='Game/g52.jpg'></div>"
-
-        $(".gameBox").append(imgMake);
-    }
+function startPlaying() {
+    $(".gameBox").empty();
+    askCard();
+    shuffle()
     return;
 }
 
-$(document).ready(function() {
+function askCard() {
+    // $(".gameBox").HTML = "";
+    let boxValue = document.querySelector('#cardPlayBox').value;
+    if (boxValue == "") {
+        cardPairs = 12;
+    }
+    else {
+        cardPairs = document.querySelector('#cardPlayBox').value;
+    }
+    
+    cardPlay = cardPairs * 2;
+    for (i = 0; i < cardPlay; i++) {
+        let imgMake = `<div class='cardBox'><img id='img${i}' class='cardClass' name='cardName' onclick='check(${i})' src='Game/g52.jpg'></div>`
+
+        $(".gameBox").append(imgMake);
+    }
+    // $('#hintButton').attr("disabled", "disabled");
+    return;
+}
+
+function loadCards() {
+    console.log("置入卡圖開始")
     let cardBankTotal = 55;
     for (let i = 0; i < 55; i++) {
         let path = "Game/g" + i + ".jpg";
         cardBank.push(path);
     }
-    console.log("")
-})
+    hasLoaded = true;
+    console.log("置入卡圖完畢")
+}
 
 function shuffle() {
+    if (hasLoaded != true) {
+        loadCards();
+    }
     console.log("洗牌開始");
-
-    for (i = 0; i < cardPlay; i++) {
-
+    for (i = 0; i < cardPairs; i++) {
         let r = Math.floor(cardNumber*Math.random());
         let tempImage = cardBank[i];
         cardBank[i] = cardBank[r];
         cardBank[r] = tempImage;
+        // console.log(cardBank[i])
     }
-
     for (let l = 0; l < cardPlay; l++) {
         tempBank[l] = "";
     }
-
-    for (let j = 0; j < cardPlay / 2; j++) {
+    for (let j = 0; j < cardPairs; j++) {
         let temp2 = cardBank[j];
         let r2 = 0;
         let upper = false;
@@ -53,8 +79,6 @@ function shuffle() {
                 upper = true;
             }
         }
-
-
         let r3 = 0;
         let lower = false;
         while (lower == false) {
@@ -62,63 +86,72 @@ function shuffle() {
             if (tempBank[r3] == "") {
                 tempBank[r3] = cardBank[j];
                 break;
-                
             }
         }
     }
+    hasStarted = false;
     hasShuffled = true;
+    $("#hintB")[0].disabled=false;
     console.log("洗牌完畢");
     return;
 }
 
-
-function hint() {
-    if (hasShuffled == false) {
-        alert("請先按[開始] 洗牌");
+function reveal () {
+    if (!hasShuffled) {
+        alert("需要先洗牌");
+        return;
     }
-    else {
-        console.log("顯示提示")
-        for (let i = 0; i < cardPlay; i++) {
-            document.cardName[i].src=tempBank[i];
-    
-        }
-        setTimeout("hide()", 2000);
+    isBusy = true;
+    for (let i = 0; i < cardPlay; i++) {
+        document.cardName[i].src=tempBank[i];
     }
+    setTimeout("hide()", 2000);
     return;
 }
 
 function hide() {
+    cardPlay = cardPairs * 2;
     for (let i = 0; i < cardPlay; i++) {
         document.cardName[i].src="Game/g52.jpg";
     } 
-    $('#hintButton').attr("disabled", "disabled");
     console.log("隱藏提示");
+    $("#hintB")[0].disabled=true;
+    isBusy = false;
     return;
 }
 
-
-let flip = false;
-let card1= 0;
-let card2= 0;
-let hasStarted = false;
-let timer1 = 0;
-let timer2 = 0;
-let now = new Date();
-
 function check(index) {
-    if (hasStarted == false) {
-        $('#hintButton').attr("disabled", "disabled");
+    if (isBusy) {
+        return;
+    }
+    if (!hasStarted) {
+        $("#hintB")[0].disabled=true;
+        let now = new Date();
         timer1 = now.getTime();
+        hasStarted = true;
         console.log(timer1);
     }
-    if (hasShuffled != true) {
+    else {
+        let now = new Date();
+        timer2 = now.getTime();
+        console.log(timer2);
+        elapsedTime = Math.floor((timer2 - timer1)/1000);
+        console.log(elapsedTime);
+        $('#timerBox')[0].value = elapsedTime;
+    }
+    if (!hasShuffled) {
         alert("請先按[開始] 洗牌");
     }
     else {
+
+
         if (flip == false) {
             document.cardName[index].src=tempBank[index];
             card1 = index;
             flip = true;
+        }
+        else if (index == card1) {
+            ;
         }
         else {
             document.cardName[index].src=tempBank[index];
@@ -131,21 +164,17 @@ function check(index) {
 }
 
 function checker(card1, card2) {
-    console.log("hit checker");
+    isBusy = true;
     source1 = document.cardName[card1].src;
     source2 = document.cardName[card2].src;
-    console.log(source1);
-    console.log(source2);
 
     if (source1 == source2) {
-        // setTimeout("correct(card1, card2);", 1000);
-        document.cardName[card1].src=cardBank[53];
-        document.cardName[card2].src=cardBank[53];
+        setTimeout("correct(card1, card2);", 500);
+        card1 = "";
+        card2 = "";
     }
     else {
-        // document.cardName[card1].src=cardBank[54];
-        // document.cardName[card2].src=cardBank[54];
-        setTimeout("incorrect(card1, card2)", 1000);        
+        setTimeout("incorrect(card1, card2)", 500);        
     }
     return;
 }
@@ -153,21 +182,42 @@ function checker(card1, card2) {
 function correct(card1, card2) {
     document.cardName[card1].src=cardBank[53];
     document.cardName[card2].src=cardBank[53];
-    setTimeout("removeImg(card1, card2)", 1000);
+    console.log(cardPairs);
+    winCount++;
+    console.log (winCount);
+    if (winCount == cardPairs) {
+        removeImg(card1, card2);
+        alert(`恭喜您浪費了您${elapsedTime}秒的寶貴時間。\n要不要再繼續浪費生命?`);
+        setTimeout("startPlaying()", 300);
+    }
+    else {
+        setTimeout("removeImg(card1, card2)", 500);
+    }
+    
 }
 
 function removeImg (card1, card2) {
-    console.log("hit remove");
-    console.log(card1 + " " + card2)
-    document.cardName[card1].css("display", "none");
-    document.cardName[card2].css("display", "none");
+    first = `img${card1}`;
+    second = `img${card2}`;
+    $(`#${first}`).css("display", "none");
+    $(`#${second}`).css("display", "none");
+    isBusy = false;
+    return;
 }
 
 function incorrect(card1, card2) {
         document.cardName[card1].src=cardBank[54];
         document.cardName[card2].src=cardBank[54];
-        setTimeout("hide()", 1000);
+        setTimeout("hide2(card1, card2)", 500);
     return;
 }
 
+function hide2(card1, card2) {
+    document.cardName[card1].src="Game/g52.jpg"
+    document.cardName[card2].src="Game/g52.jpg"
+    isBusy = false;
+    return
+}
+
 askCard();
+startPlaying();
