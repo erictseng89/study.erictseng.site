5d52aac8ebb3948013781d222357ad5c0e6e8013
Eric Tseng <erictseng89@gmail.com>
2021-04-15 14:11:54 +0800

    fixed chatroom

--- gmaps/chatroom.js
+++ gmaps/chatroom.js
@@ -2,78 +2,6 @@
 
 // Must use var instead of let here.
 var firebase;
-$( () => {
-    let 
-        userName = $('#name'), 
-        message = $('#message'), 
-        sendButton = $('#sendButton'),
-        chat = $('#chatContents'),
-        ms = new Date().getTime();
-
-    var config = {
-        //databaseURL: "https://test0504-e0273.firebaseio.com/" //你的資料庫名稱
-        databaseURL: "https://maps-chatbox-default-rtdb.firebaseio.com/"
-    };
-/*     let firebaseConfig = {
-        databaseURL: "https://maps-chatbox-default-rtdb.firebaseio.com/"
-    }; */
-
-    firebase.initializeApp(config);
-    let database = firebase.database().ref();
-
-    sendButton.on('click', write);
-
-    message.on('keydown', (key) => {
-        if (key.keyCode == 13) {
-            write();
-        }
-    });
-
-    function write() {
-        let date = new Date();
-        let
-            h = date.getHours(),
-            m = date.getMinutes(),
-            s = date.getSeconds();
-
-        h = h < 10 ? '0' + h : h;
-        m = m < 10 ? '0' + m : m;
-        s = s < 10 ? '0' + s : s;
-
-        let timeNow = `${h} : ${m} : ${s}`;
-
-        let postData = {
-            name:userName.val(),
-            content:message.val(),
-            time:timeNow,
-            id:'id'+ms
-        };
-
-        database.push(postData);
-        message.val('');
-    }
-
-    database.once('value', (snapshot) => {
-        chat.html('');
-        for (let i in snapshot.val()) {
-            let c = snapshot.val()[i];
-            chat.prepend(`<div><div>${c.time}</div>${c.name} 說: ${c.content}</div>`)
-        }
-/*         snapshot.val().forEach( (a) => {
-            chat.prepend(`<div><div>${a.time}</div> ${a.name} 說: ${a.content}</div>`);
-        }) */
-    });
-
-    database.limitToLast(1).on('value', (snapshot) => {
-        for (let i in snapshot.val()) {
-            let c = snapshot.val()[i];
-            chat.prepend(`<div class="${c.id}"><div>${c.time}</div>${c.name} 說: ${c.content}</div>`);
-        }
-/*         snapshot.val().forEach( (a) => {
-            chat.prepend(`<div class="${a.id}"><div>${a.time}</div>${a.name} 說: ${a.content}</div>`); 
-        }) */
-    });
-});
 
 
 
@@ -82,8 +10,6 @@ $( () => {
 
 /* let pos;
 let map;
-let messages = document.getElementById("chat_contents").value;
-let people = []; */
 
 /* function send {
     let name = document.getElementById("name").value;
@@ -113,6 +39,31 @@ if (navigator.geolocation) {
 else {
   alert ("定位發生錯誤")
 }
+*/
+
+
+
+/* function locate() {
+    let pos;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition( (position) => {
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            }
+            console.log(pos);
+        })
+    }
+    else {
+      alert ("定位發生錯誤")
+    }
+    alert(pos);
+    return pos;
+}
+ */
+
+
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -120,5 +71,137 @@ function initMap() {
         // center: pos,
         zoom: 14,
     });
-} */
+
+    const icons = [{
+        url: 'https://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/600px-001Bulbasaur.png',
+        scaledSize: new google.maps.Size(50,50)
+    }, {
+        url: 'https://cdn.bulbagarden.net/upload/thumb/7/73/004Charmander.png/600px-004Charmander.png',
+        scaledSize: new google.maps.Size(50,50)
+    }, {
+        url: "https://cdn.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png", 
+        scaledSize: new google.maps.Size(50, 50)
+    }];
+
+    let newMarker;
+    $( () => {
+        let 
+            userName = $('#name'), 
+            message = $('#message'), 
+            sendButton = $('#sendButton'),
+            deleteButton = $('#deleteButton'),
+            chat = $('#chatContents'),
+            ms = new Date().getTime();
+    
+        var config = {
+            databaseURL: "https://maps-chatbox-default-rtdb.firebaseio.com/"
+        };
+    
+    
+        firebase.initializeApp(config);
+        let database = firebase.database().ref();
+    
+
+
+        message.on('keydown', (key) => {
+            if (key.keyCode == 13) {
+
+
+                write($('#message').val());
+        
+                
+                // newMarker = {
+                //     position: msgPos,
+                //     map: map,
+                // }
+                // newMarker = new google.maps.Marker(e);
+                // newMarker.setMap(map);
+
+                // map.setCenter(msgPos);
+            }
+        });
+    
+        function write() {
+            console.log($('#message').val());
+            let comment = $('#message').val();
+            let date = new Date();
+            let
+                h = date.getHours(),
+                m = date.getMinutes(),
+                s = date.getSeconds();
+    
+            h = h < 10 ? '0' + h : h;
+            m = m < 10 ? '0' + m : m;
+            s = s < 10 ? '0' + s : s;
+    
+            let timeNow = `${h}:${m}:${s}`;
+    
+            let pos;
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition( (position) => {
+                    pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    }
+                    // console.log(pos);
+                    let marker = new google.maps.Marker({
+                        position: pos,
+                        map: map,
+                        icon: icons[$('#pokemon').val()]
+                    });
+                    marker.setMap(map);
+
+                    map.panTo(pos);
+                    infoWindow = new google.maps.InfoWindow();
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent(comment);
+                    infoWindow.open(map, marker);
+                })
+            }
+
+            else {
+              alert ("定位發生錯誤")
+            }
+
+
+            let postData = {
+                name:userName.val(),
+                content:message.val(),
+                time:timeNow,
+                id:'id'+ms
+            };
+          
+    
+            database.push(postData);
+            message.val('');
+        }
+    
+        function remove() {
+            console.log("remove");
+            database.remove();
+            chat.html('');
+        }
+
+        database.once('value', (snapshot) => {
+            chat.html('');
+
+            // Tried to use snapshot.val().forEach, but it didnt work here.
+            for (let i in snapshot.val()) {
+                let c = snapshot.val()[i];
+                chat.prepend(`<div><span>${c.time}</span> | ${c.name}: ${c.content}</div>`)
+
+            }
+        });
+    
+        database.limitToLast(1).on('value', (snapshot) => {
+            for (let i in snapshot.val()) {
+                let c = snapshot.val()[i];
+                chat.prepend(`<div class="${c.id}"><span>${c.time} | </span>${c.name}: ${c.content}</div>`);
+            }
+        });
+
+        sendButton.on('click', write);
+        deleteButton.on('click', remove);
+    });
+} 
 
